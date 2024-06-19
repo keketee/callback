d89b98bc27c9f95a19e16a51ce677b4ff72f18e0
ecerrolarios <elgrankeko@hotmail.com>
2024-06-19 18:56:16 +0200

    UPDATE

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,9 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+
+//callback -> funcion que ejecuta otra funcion
+
+const letters = ['a', 'b', 'c ', 'd']
+
+letters.forEach(letter => console.log (letter))
